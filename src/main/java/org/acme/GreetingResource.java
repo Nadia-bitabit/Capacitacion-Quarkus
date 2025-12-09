b834bd08a7c8a1f43bafa1df7cba323d5b13642c
Nadia-bitabit <nadia@bitabit.tech>
2025-12-09 18:40:49 -0300

    Creacion de endpoint

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,12 +5,32 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/saludar")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
     }
+
+    @GET
+    @Path("/dia")
+    public String saludaDias() {
+        return "Buenos dias";
+    }
+
+    @GET
+    @Path("/tarde")
+    public String saludaTarde() {
+        return "Buenos Tarde";
+    }
+
+    @GET
+    @Path("/noche")
+    public String saludaNoche() {
+        return "Buenos Noche";
+    }
+
+
+
 }
